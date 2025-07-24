4e80719bb33bbf6d2e7d21880ce69eabab84c5e6
Arpan-das-dev <dasarpan915@gmail.com>
2025-07-24 11:26:01 +0530

    checking if a phrase is a valid palindrome or not

--- src/main/java/org/solutions/ValidPalindrome.java
+++ src/main/java/org/solutions/ValidPalindrome.java
@@ -2,10 +2,18 @@ package org.solutions;
 
 public class ValidPalindrome {
     public boolean isValidPalindrome(String s) {
-        if (s.length() == 0) return false;
-        int right = 0;
-        int left = s.length() - 1;
-
-        return false;
+        if (s.isEmpty()) return false;
+        int right = s.length() - 1;
+        int left = 0;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            Character leftChar = Character.toLowerCase(s.charAt(left));
+            Character rightChar = Character.toLowerCase(s.charAt(right));
+            if (leftChar != rightChar) return false;
+            left++;
+            right--;
+        }
+        return true;
     }
 }
