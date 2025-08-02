bfdf04cdad6adecda98b8ea6b505f18d8a6d48bd
Arpan Das <dasarpan915@gmail.com>
2025-08-02 22:01:35 +0530

    Using two pointers approach to find if the string is palindrome or not

--- src/main/java/org/solutions/ValidPalindrome.java
+++ src/main/java/org/solutions/ValidPalindrome.java
@@ -8,8 +8,10 @@ public class ValidPalindrome {
         while (left < right) {
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            
             Character leftChar = Character.toLowerCase(s.charAt(left));
             Character rightChar = Character.toLowerCase(s.charAt(right));
+            
             if (leftChar != rightChar) return false;
             left++;
             right--;
