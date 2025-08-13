c312a4473671fbe5f627034ac37c2e50aa1fb730
Arpan Das <dasarpan915@gmail.com>
2025-08-13 22:20:38 +0530

    Two sum solution using two pointers

--- src/main/java/org/solutions/twoSum2.java
+++ src/main/java/org/solutions/twoSum2.java
@@ -6,7 +6,6 @@ public class twoSum2 {
         int left = 0;
         int right = arr.length - 1;
 
-        
         while (left < right) {
             int sum = arr[left] + arr[right];
             if (sum == target) {
